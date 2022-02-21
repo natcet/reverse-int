0f332af7c75931b2b8169f4402d2c54189e2704e
Natallia Markava <natallia.markava@newland.by>
2022-02-21 14:26:02 +0300

    solution reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+   
+    if (n < 0) {
+
+        n = n * (-1);
+    }
+
+    let numToStr = String(n);
+    
+    let i = numToStr.length - 1;
+
+    let totalStr = '';
+    
+        while(i >= 0) {
+    
+            totalStr = `${totalStr}${numToStr[i]}`;
+            
+            i = i - 1;
+
+        }
+    
+    let strToNum = Number(totalStr);
+
+    return strToNum;
+    
 }
+
+
+        
+
